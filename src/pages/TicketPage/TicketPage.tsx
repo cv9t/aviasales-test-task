62e6273940e8ed1e24986bc092c39ea085df8fa4
Svyatoslav <sviatoslavnazin@gmail.com>
2021-10-03 22:40:55 +0800

    Fix bugs

--- src/pages/TicketPage/TicketPage.tsx
+++ src/pages/TicketPage/TicketPage.tsx
@@ -14,69 +14,69 @@ import { animateScroll as scroll } from "react-scroll";
 import MyScroll from "../../components/UI/Scroll/MyScroll";
 
 const TicketPage = () => {
-	const [tickets, setTickets] = useState<ITicket[]>([]);
-	const [filter, setFilter] = useState<IFilter>({ chx: [], sort: "" });
-	const [ticketsLimit, setTicketsLimit] = useState<number>(5);
-	const [showScrollBtn, setShowScrollBtn] = useState<boolean>(false);
-	const sortedAndFilteredTickets: ITicket[] = useTickets(
-		tickets,
-		filter.sort,
-		filter.chx,
-	);
+    const [tickets, setTickets] = useState<ITicket[]>([]);
+    const [filter, setFilter] = useState<IFilter>({ chx: [], sort: "" });
+    const [ticketsLimit, setTicketsLimit] = useState<number>(5);
+    const [showScrollBtn, setShowScrollBtn] = useState<boolean>(false);
+    const sortedAndFilteredTickets: ITicket[] = useTickets(
+        tickets,
+        filter.sort,
+        filter.chx,
+    );
 
-	const [fetchTickets, isTicketsLoading] = useFetching(async () => {
-		const response = await TicketService.getAll();
-		setTickets([...response]);
-	});
+    const [fetchTickets, isTicketsLoading] = useFetching(async () => {
+        const response = await TicketService.getAll();
+        setTickets([...response]);
+    });
 
-	useEffect(() => {
-		setTicketsLimit(5);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [filter]);
+    useEffect(() => {
+        setTicketsLimit(5);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [filter]);
 
-	useEffect(() => {
-		fetchTickets();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+    useEffect(() => {
+        fetchTickets();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-	window.addEventListener("scroll", () => {
-		if (window.scrollY >= 300) {
-			setShowScrollBtn(true);
-		} else {
-			setShowScrollBtn(false);
-		}
-	});
+    window.addEventListener("scroll", () => {
+        if (window.scrollY >= 300) {
+            setShowScrollBtn(true);
+        } else {
+            setShowScrollBtn(false);
+        }
+    });
 
-	return (
-		<div className={cl.TicketPage}>
-			<Filter
-				title="Количество пересадок"
-				filter={filter}
-				onFilterChange={setFilter}
-			/>
-			<div className={cl.TicketPage__container}>
-				<Tabs filter={filter} onSortChange={setFilter} />
-				{isTicketsLoading && <Loader />}
-				<List
-					items={sortedAndFilteredTickets.filter(
-						(i, index) => index < ticketsLimit,
-					)}
-					renderItem={(ticket: ITicket, index) => (
-						<Ticket item={ticket} key={`ticket-${index}`} />
-					)}
-				/>
-				{ticketsLimit <= sortedAndFilteredTickets.length && (
-					<MyButton onClick={() => setTicketsLimit(ticketsLimit + 5)}>
-						показать еще 5 билетов!
-					</MyButton>
-				)}
-				<MyScroll
-					isShow={showScrollBtn}
-					onClick={() => scroll.scrollToTop()}
-				/>
-			</div>
-		</div>
-	);
+    return (
+        <div className={cl.TicketPage}>
+            <Filter
+                title="Количество пересадок"
+                filter={filter}
+                onFilterChange={setFilter}
+            />
+            <div className={cl.TicketPage__container}>
+                <Tabs filter={filter} onSortChange={setFilter} />
+                {isTicketsLoading && <Loader />}
+                <List
+                    items={sortedAndFilteredTickets.filter(
+                        (i, index) => index < ticketsLimit,
+                    )}
+                    renderItem={(ticket: ITicket, index) => (
+                        <Ticket item={ticket} key={`ticket-${index}`} />
+                    )}
+                />
+                {ticketsLimit < sortedAndFilteredTickets.length && (
+                    <MyButton onClick={() => setTicketsLimit(ticketsLimit + 5)}>
+                        показать еще 5 билетов!
+                    </MyButton>
+                )}
+                <MyScroll
+                    isShow={showScrollBtn}
+                    onClick={() => scroll.scrollToTop()}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default TicketPage;
